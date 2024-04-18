57486c95d652bd2345b628430467245f25685069
María Victoria <56141354+mvalenciaar@users.noreply.github.com>
2024-04-18 11:16:19 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -49,4 +49,4 @@
 -- ON tbl1.K1 = tbl2.K1
 -- WHERE c13 > 400
 -- GROUP BY K0
-SELECT K0, ROUND(avg(c21),2) AS avg(c21) FROM tbl1 t1, tbl2 t2 WHERE t1.K1 = t2.K1 AND c13 > 400 GROUP BY K0
+SELECT K0, ROUND(avg(c21),2) AS "avg(c21)" FROM tbl1 t1, tbl2 t2 WHERE t1.K1 = t2.K1 AND c13 > 400 GROUP BY K0
